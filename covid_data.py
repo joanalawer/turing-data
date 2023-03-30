2dee7db7a95654710c99485de6a0fe6676e9e190
joanalawer <lawerjoana477@gmail.com>
2023-03-30 17:45:16 +0000

    Renamed file

---
+++ covid_data.py
@@ -0,0 +1,22 @@
+# Python Data Analysis Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# %matplotlib inline
+
+# Overview of Dataset
+alco = pd.read_csv("cardio_alco.csv")
+alco.head()
+
+alco.shape
+
+alco.info()
+
+alco.describe()
+
+# Numerical analysis and visualisation
+#Analyse on a column
+alco['']
+
+# Categorical analyses and visualisation
